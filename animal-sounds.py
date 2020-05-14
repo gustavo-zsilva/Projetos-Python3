285ff10b9eb0f6b45c0286654eb6aefcb96a8627
Gustavo Zonta da Silva <65104544+gustavo-zsilva@users.noreply.github.com>
2020-05-14 00:02:23 -0300

    adicionei comentários ao animal-sounds

--- animal-sounds.py
+++ animal-sounds.py
@@ -1,11 +1,14 @@
 import random
 
+# Lista dos animais e seu som correspondente.
 animais = ['Tigre', 'Leão', 'Cobra', 'Pássaro']
 sons = ['Rawr', 'Groar', 'Ssss', 'Chirp']
 
+# Randomizar a lista dos sons.
 random = random.sample(sons, 4)
 print(random)
 
+# Iterar vários testes do som correspondente ao animal.
 for c in range(0, 4) :
     if random[c] == sons[0]:
         print(animais[0])
@@ -15,7 +18,3 @@ for c in range(0, 4) :
         print(animais[2])
     else:
         print(animais[3])
-
-
-
-
